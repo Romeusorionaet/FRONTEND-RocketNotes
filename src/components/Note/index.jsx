f315fe111c8a0f8cb36f07aa05695480949a02c0
romeusorionaet <romeusoares14569@gmail.com>
2022-12-26 14:15:18 -0300

    commit

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -10,11 +10,9 @@ export function Note({data, ...rest}) {
                 data.tags &&
                 <footer>
                     {
-                    data.tags.map(tag=> 
-                    <Tags 
-                    key={tag.id} 
-                    title={tag.name} 
-                    />)
+                    data.tags.map(tag =>(
+                    <Tags key={String(tag.id)} title={tag.name} />
+                    ))
                     }
                 </footer>
             }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -79,7 +79,7 @@ export function Details() {
           }
 
           {
-            data.links &&
+            data.tags &&
             <Section title="Marcadores">
             {
               data.tags.map(tag=>(
